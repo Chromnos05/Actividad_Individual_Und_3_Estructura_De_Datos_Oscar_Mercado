7aa38b3bb95c295fe09ae40932339fee56f96e09
Oscar Mercado <omercador@unicartagena.edu.co>
2026-06-05 22:20:34 -0500

    cargar proyecto inicializado

---
+++ Taller_Computadores/src/main/java/com/mycompany/taller_computadores/Taller_Computadores.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.taller_computadores;
+
+/**
+ *
+ * @author Oscar M
+ */
+public class Taller_Computadores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
